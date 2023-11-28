14b7f2cfede69cb213357cbaa99692be3d00971d
Emanuel Berger <emanuelberger49@gmail.com>
2023-11-28 10:28:32 +0100

    some tests

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import os
 from collections import Counter
 
-# Funktion zum Zählen der Übereinstimmungen von Wörtern in einem Text
+# Funktion zum Zählen der Übereinstimmungen von Wörtern in einem Text (case-insensitiv)
 def count_matches(word_list, text):
-    return sum(1 for word in word_list if word in text)
+    return sum(1 for word in word_list if word.lower() in text.lower())
 
-# Funktion zum Extrahieren von Lateinischen Wörtern aus einer Textdatei
+# Funktion zum Extrahieren von Lateinischen Wörtern aus einer Textdatei (case-insensitiv)
 def extract_latin_words(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
-        return [word.strip() for word in file.readlines()]
+        return [word.strip().lower() for word in file.readlines()]
 
 # Funktion zum Suchen des Briefs mit den meisten Übereinstimmungen
 def find_most_matching_letter(word_list, letter_directory):
@@ -30,7 +30,7 @@ def main():
     latin_words_file = 'latin_words.txt'  # Dateipfad zur Datei mit Lateinischen Wörtern
     plinius_letters_directory = 'plinius_letters'  # Ordner mit den Plinius-Briefen
 
-    # Lateinische Wörter extrahieren
+    # Lateinische Wörter extrahieren (case-insensitiv)
     latin_words = extract_latin_words(latin_words_file)
 
     # Den Brief mit den meisten Übereinstimmungen finden
